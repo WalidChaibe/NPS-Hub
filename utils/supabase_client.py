e0d49a71f42cc00b3d0c11f3376b0e21ce2bf5cf
WalidChaibe <walid.elchaibe@napconational.com>
2026-03-06 04:19:03 +0300

    Update supabase_client.py

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -6,3 +6,9 @@ def get_supabase() -> Client:
     url = st.secrets["SUPABASE_URL"]
     key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
+
+def get_supabase_admin():
+    from supabase import create_client
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_SERVICE_KEY"]
+    return create_client(url, key)
